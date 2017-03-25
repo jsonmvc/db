219f55209680098c15373ee5256d673ad219dd51
dumconstantin <dum.constantin@gmail.com>
2017-03-25 04:01:39 +0200

    Remove comment in invalidateCache about the current implementation.

--- src/fn/invalidateCache.js
+++ src/fn/invalidateCache.js
@@ -2,32 +2,6 @@
 const splitPath = require('./splitPath')
 const decomposePath = require('./decomposePath')
 
-// @TODO: Add precomputation in expandNodeDeps to calculate all the static nodes
-// upon which the dynamic nodes depens
-// @TODO: Change set cache to add dynamic nodes differently:
-// e.g. if a dynamic node is /foo and the path /foo/bar is requested
-// then the cache will be store:
-// /foo: {
-//    /foo/bar: 123
-// }
-//
-// Implementation:
-// if (cache[path]) {
-//    return cache[path]
-// } else if (cache[rootOfDynamicNode(path)]) {
-//    return cache[rootOfDynamicNode(path)][path]
-// } else {
-//    -> compute value
-// }
-//
-// Doing so when deleting a dynamic node no iteration on children node
-// is needed. Just a clear delete on the node ref.
-//
-// To optimize this further create a single ref spaning from the static node that
-// all the dynamic nodes are listening to and delete only that.
-//
-// Implementing the same for listeners!
-
 function invalidateCache(db, changed) {
 
   changed.full.forEach(x => {
